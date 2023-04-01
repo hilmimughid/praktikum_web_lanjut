e793f54f6ca62aaa54829f272a4b65f08e5da781
hilmimughid <113970790+hilmimughid@users.noreply.github.com>
2023-04-01 19:51:20 +0700

    Praktikum 2

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,21 @@
+<div class="container">
+    <div class="row">
+        <div class="tm-top-header-inner">
+            <div class="tm-logo-container">
+                <img src="img/logo.png" alt="Logo" class="tm-site-logo">
+                <h1 class="tm-site-name tm-handwriting-font">Cafe House</h1>
+            </div>
+            <div class="mobile-menu-icon">
+                <i class="fa fa-bars"></i>
+            </div>
+            <nav class="tm-nav">
+                <ul>
+                    <li><a href="/home">Home</a></li>
+                    <li><a href="/today-special">Today Special</a></li>
+                    <li><a href="/menu">Menu</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/today-special', function () {
+    return view('today-special');
+});
+
+Route::get('/menu', function () {
+    return view('menu');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
