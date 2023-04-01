f4c1550397c23d96671e827280cf85c3369e0125
hilmimughid <113970790+hilmimughid@users.noreply.github.com>
2023-04-01 21:32:14 +0700

    Praktikum 1

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -10,7 +10,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Hilmi Mughid | 2141720081</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
